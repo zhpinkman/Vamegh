2edb5ee37e4826dfa7e0a2369ef79e8739d77beb
sh-soltanpour <shahryar2006@gmail.com>
2018-08-02 12:23:11 +0430

    add userService to auth manager (For refreshToken bug)

--- src/main/kotlin/ir/ac/ut/acm/storage/vamegh/VameghApplication.kt
+++ src/main/kotlin/ir/ac/ut/acm/storage/vamegh/VameghApplication.kt
@@ -1,10 +1,29 @@
 package ir.ac.ut.acm.storage.vamegh
 
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.config.BeanIds
+
+
 
 @SpringBootApplication
-class VameghApplication
+class VameghApplication {
+
+    @Autowired
+    lateinit var userService: UserDetailsService
+
+    @Autowired
+    @Throws(Exception::class)
+    fun authenticationManager(builder: AuthenticationManagerBuilder) {
+        builder.userDetailsService<UserDetailsService>(userService)
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<VameghApplication>(*args)
