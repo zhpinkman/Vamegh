c4d8a819d77f827f9056db738a283c9336673417
zhivar <zh.sourati@ut.ac.ir>
2018-08-02 13:31:00 +0430

    uploudFileService added

--- src/main/kotlin/ir/ac/ut/acm/storage/vamegh/VameghApplication.kt
+++ src/main/kotlin/ir/ac/ut/acm/storage/vamegh/VameghApplication.kt
@@ -1,8 +1,11 @@
 package ir.ac.ut.acm.storage.vamegh
 
+import com.example.demo.FileStorage
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.core.userdetails.UserDetailsService
 
@@ -12,12 +15,21 @@ class VameghApplication {
 
     @Autowired
     lateinit var userService: UserDetailsService
+    @Autowired
+    lateinit var fileStorage: FileStorage;
 
     @Autowired
     @Throws(Exception::class)
     fun authenticationManager(builder: AuthenticationManagerBuilder) {
         builder.userDetailsService<UserDetailsService>(userService)
     }
+
+    @Bean
+    fun run() = CommandLineRunner {
+        fileStorage.deleteAll()
+        fileStorage.init()
+    }
+
 }
 
 fun main(args: Array<String>) {
