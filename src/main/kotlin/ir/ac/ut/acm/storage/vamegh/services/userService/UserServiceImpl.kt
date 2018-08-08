07c5c191a8516e87e81a1bd496f30813cbb5ec0e
zhivar <zh.sourati@ut.ac.ir>
2018-08-08 22:41:11 +0430

    throwing exceptions for duplicateKey corrected

--- src/main/kotlin/ir/ac/ut/acm/storage/vamegh/services/userService/UserServiceImpl.kt
+++ src/main/kotlin/ir/ac/ut/acm/storage/vamegh/services/userService/UserServiceImpl.kt
@@ -2,14 +2,15 @@ package ir.ac.ut.acm.storage.vamegh.services.userService
 
 import ir.ac.ut.acm.storage.vamegh.exceptions.EntityNotFound
 import ir.ac.ut.acm.storage.vamegh.exceptions.InvalidEmailException
-import ir.ac.ut.acm.storage.vamegh.exceptions.NotUniqueException
 import ir.ac.ut.acm.storage.vamegh.configurations.PasswordEncoder
 import ir.ac.ut.acm.storage.vamegh.controllers.user.models.RegisterRequest
 import ir.ac.ut.acm.storage.vamegh.entities.User
+import ir.ac.ut.acm.storage.vamegh.exceptions.NotUniqueException
 import ir.ac.ut.acm.storage.vamegh.repositories.UserRepository
 import ir.ac.ut.acm.storage.vamegh.services.fileService.FileStorageService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.dao.DuplicateKeyException
 import org.springframework.stereotype.Service
 
 
@@ -39,11 +40,8 @@ class UserServiceImpl: UserService {
             val user = User(email = registerRequest.email.toLowerCase(), bucketName = registerRequest.bucketName, password = passwordEncoded)
             userRepository.save(user)
             fileStorageService.mkDir(rootLocation + "/" + registerRequest.bucketName)
-
-        }catch (e: InvalidEmailException){
-            throw e
         }
-        catch (e: Exception) {
+        catch (e: DuplicateKeyException) {
             throw NotUniqueException()
         }
 
